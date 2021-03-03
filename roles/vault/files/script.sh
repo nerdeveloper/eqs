5a6b7acccee885b3f0fa0d73c89a233f2ca68a93
nerdeveloper <odirionye@gmail.com>
2021-03-03 01:21:24 +0100

    chore: update pod injection

--- roles/vault/files/script.sh
+++ roles/vault/files/script.sh
@@ -1,19 +1,28 @@
-kubectl exec -it vault-0 -- vault secrets enable -path=internal kv-v2
-kubectl exec -it vault-0 -- vault kv put internal/database/config username="{{ username }}" password="{{ password}}"
-kubectl exec -it vault-0 -- vault kv get internal/database/config
+kubectl exec -it vault-0 -- vault secrets enable -path=secret kv-v2
+kubectl exec -it vault-0 -- vault kv put secret/database password="admin"
+kubectl exec -it vault-0 -- vault kv get secret/database
 kubectl exec -it vault-0 -- vault auth enable kubernetes
-kubectl exec -it vault-0 -- vault write auth/kubernetes/config token_reviewer_jwt="$(kubectl exec -it vault-0 -- cat /var/run/secrets/kubernetes.io/serviceaccount/token)" kubernetes_host="https://$KUBERNETES_PORT_443_TCP_ADDR:443" kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+# shellcheck disable=SC2034
+k8s_host="$(kubectl config view --minify | grep server | cut -f 2- -d ":" | tr -d " ")"
 
-kubectl exec -it vault-0 -- vault policy write internal-app - <<EOH
-path "internal/data/database/config" {
+secret_name="$(kubectl get serviceaccount mysql -o go-template='{{ (index .secrets 0).name }}')"
+# shellcheck disable=SC2034
+k8s_cacert="$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}' | base64 --decode)"
+
+tr_account_token="$(kubectl get secret mysql  -o go-template='{{ .data.token }}' | base64 --decode)"
+
+kubectl exec -i vault-0 -- vault write auth/cluster-01/config token_reviewer_jwt="${tr_account_token}" kubernetes_host="${k8s_host}" kubernetes_ca_cert="${k8s_cacert}"
+
+kubectl exec -i vault-0 -- vault policy write mysql - <<EOH
+path "secret*" {
   capabilities = ["read"]
 }
 EOH
 
-kubectl exec -it vault-0 -- vault write auth/kubernetes/role/internal-app \
-          bound_service_account_names=internal-app \
+kubectl exec -it vault-0 -- vault write auth/kubernetes/role/mysql \
+          bound_service_account_names=mysql \
           bound_service_account_namespaces=default \
-          policies=internal-app \
-          ttl=24h
+          policies=mysql \
+          ttl=24h \
+          token_max_ttl=42h
 
-kubectl create serviceaccount internal-app
